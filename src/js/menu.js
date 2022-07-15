acd05b20a06ddb0955085f0e6c0161afb98c98bb
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-07-15 15:37:58 +0300

    tab styles  ready

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,6 +4,7 @@
     openMenuBtn: document.querySelector('.header__mobile-menu-open-btn'),
     closeMenuBtn: document.querySelector('.mobile-menu-close-btn'),
     menu: document.querySelector('.mobile-menu'),
+    hide: document.querySelector('.hide'),
   };
   console.log(refs);
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -12,5 +13,6 @@
   function toggleMenu() {
     refs.body.classList.toggle('no-scroll');
     refs.menu.classList.toggle('not-visible');
+    refs.hide.classList.toggle('is-hidden');
   }
 })();
