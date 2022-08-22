10cfaca7cc34547611ff722744a89f6553cf7cce
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-08-22 14:05:48 +0300

    add open button for video chat

--- src/js/video-call.js
+++ src/js/video-call.js
@@ -1,7 +1,7 @@
 callFrame = window.DailyIframe.createFrame({
   iframeStyle: {
     position: 'fixed',
-    border: '1px solid black',
+    // border: '1px solid black',
     width: '375px',
     height: '450px',
     right: '1em',
@@ -12,5 +12,15 @@ callFrame = window.DailyIframe.createFrame({
   },
   showLeaveButton: true,
   showFullscreenButton: true,
+  lang: 'user',
 });
 callFrame.join({ url: 'https://super-company.daily.co/super-company' });
+
+const openCallBtn = document.querySelector('.openVideoChat');
+const iframeRef = document.querySelectorAll('iframe')[length - 1];
+
+iframeRef.classList.add('is-hidden');
+
+openCallBtn.addEventListener('click', () =>
+  iframeRef.classList.toggle('is-hidden')
+);
